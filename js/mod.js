f5155e0359cac8d280da9c3880cf99c3824c4b92
rodan12 <141339442+Glennreble@users.noreply.github.com>
2024-05-01 18:21:50 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -48,71 +48,71 @@ const mod = {
             "01",
             "2345"
         ]],
-        ["Morse code (JeehanMoment)",
+        ["Morse code",
         [
             [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."],
             ["-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."],
             ["..--..", ".-...", "........", "...-."]
         ]],
-        ["Wingdings (JeehanMoment)",
+        ["Wingdings",
         [
             '♋♌♍♎♏♐♑♒♓🙰🙵●🔾■□🞐❑❒⬧⧫◆❖⬥⌧⮹⌘',
             "📁📂📄🗏🗐🗄⌛🖮🖰🖲",
             "✌👌👍👎"
         ]],
-        ["Unary (JeehanMoment)",
+        ["Unary",
         [
             '1',
             "1",
             "2345"
         ]],
-        ["Who's on first? (JeehanMoment)",
+        ["Who's on first?",
         [
             ["YES", "FIRST", "DISPLAY", "OKAY", "SAYS", "NOTHING", "", "BLANK", "NO", "LED", "LEAD", "READ", "RED", "REED", "LEED", "HOLD ON", "YOU", "YOU ARE", "YOUR", "YOU'RE", "UR", "THERE", "THEY'RE", "THEIR", "THEY ARE", "SEE", "C", "CEE"],
             ["READY", "FIRST", "NO", "BLANK", "NOTHING", "YES", "WHAT", "UHHH", "LEFT", "RIGHT", "MIDDLE", "OKAY", "WAIT", "PRESS", "YOU", "YOU'RE", "UR", "U", "UH HUH", "UH UH", "WHAT?", "DONE", "NEXT", "HOLD", "SURE", "LIKE"],
             ["WHO'S", "ON", "FIRST", "?"]
         ]],
-        ["Chess (JeehanMoment)",
+        ["Chess",
         [
             ["♙", "♘", "♗", "♖", "♕", "♔", "♟", "♞", "♝", "♜", "♛", "♚", "O-O", "O-O-O"],
             ["a1", "a2", "a3", "a4", "a5", "a6" , "a7", "a8", "b1", "b2", "b3", "b4", "b5", "b6" , "b7", "b8", "c1", "c2", "c3", "c4", "c5", "c6" , "c7", "c8", "d1", "d2", "d3", "d4", "d5", "d6" , "d7", "d8", "e1", "e2", "e3", "e4", "e5", "e6" , "e7", "e8", "f1", "f2", "f3", "f4", "f5", "f6" , "f7", "f8", "g1", "g2", "g3", "g4", "g5", "g6" , "g7", "g8", "h1", "h2", "h3", "h4", "h5", "h6" , "h7", "h8"],
             ["x", "+", "=", "#"]
         ]],
-        ["Confusion (JeehanMoment)",
+        ["Confusion",
         [
             ["1", "10", "11", "20", "21", "100", "101", "110", "111", "120", "121", "200", "201", "210", "211", "220", "221", "300", "301", "310", "311", "320", "321"],
             ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot" , "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November" , "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor" , "Whiskey", "X-ray", "Yankee", "Zulu"],
             ["?", "!", ".", "-"]
         ]],
-        ["Minecraft (Nif)",
+        ["Minecraft",
         [
             ["Oak", "Spruce", "Birch", "Jungle", "Acacia", "Dark oak", "Crimson", "Warped", "Mangrove", "Bamboo", "Crafting table"],
             ["Coal", "Copper", "Amethyst", "Lapis", "Iron", "Gold", "Redstone", "Emerald", "Diamond", "Quartz", "Netherrite"],
             ["Overworld", "Nether", "End", "Aether"]
         ]
         ]],
-        ["Minecraft shorthand (Nif)",
+        ["Minecraft shorthand",
         [
             ["O", "S", "B", "J", "A", "DO", "C", "W", "M", "B", "CT"],
             ["Co", "Cu", "Am", "L", "I", "G", "R", "E", "D", "Q", "N"],
             ["Ov", "Ne", "En", "Ae"]
         ]
         ],
-        ["Elemental (Nif)",
+        ["Elemental",
         [
             ["Neutronium", "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon", "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium", "Aluminium", "Silicon", "Phosphorous", "Sulfur", "Chlorine", "Argon", "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium", "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium", "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium", "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenium", "Technecium", "Rutherfordium", "Rhodium", "Palladium", "Silver", "Cadmium", "Indium", "Tin", "Antimony", "Tellerium", "Iodine", "Xenon", "Caesium", "Barium", "Lanthanium", "Cerium", "Praseodymium", "Neodymium", "Promethium", "Samarium", "Europeum", "Gadolinium", "Terbium", "Dysprosium", "Holomium", "Erbium", "Thulium", "Ytterbium", "Lutetium", "Hafnium", "Tantalium", "Tungsten", "Rhenium", "Osmium", "Iridium", "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth", "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium", "Thorium", "Proactinium", "Uranium", "Neptunium", "Plutonium", "Americium", "Curium", "Berkelium", "Californium", "Einsteinium", "Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium", "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium", "Darmstadtium", "Rogentgenium", "Copernicium", "Nihonium", "Flerovium", "Moscovium", "Livermorium", "Tenessine", "Oganesson"],
             ["Up quark", "Down quark", "Charm quark", "Strange quark", "Top quark", "Bottom quark", "Electron", "Muon", "Tau particle", "Electron neutrino", "Muon neutrino", "Tau neutrino", "Gluon", "Photon", "Z boson", "W boson", "Higgs boson"],
             ["Delta 2+", "Delta proton", "Delta neutron", "Delta electron"]
         ]
         ],
-        ["Elemental shorthand (Nif)",
+        ["Elemental shorthand",
         [
             ["Nt", "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "Ar", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"],
             ["u", "d", "c", "s", "t", "b", "e", "μ", "τ", "Ve", "Vμ", "Vτ", "g", "γ", "Z", "W", "H"],
             ["Δ⁺⁺", "Δ⁺", "Δ⁰", "Δ⁻"]
         ]
         ],
-        ["Elemental (jwklong's edition)",
+        ["Elemental by jwklong",
         [
             ["H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe","Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn","Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"],
             ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100","101","102","103","104","105","106","107","108","109","110","111","112","113","114","115","116","117","118"],
@@ -125,14 +125,14 @@ const mod = {
             Utils.createRandomWord(10, new Random(Math.floor(Date.now()/2)).nextInt()),
             [Utils.createRandomWord(2, new Random(Math.floor(Date.now()/3)).nextInt()),Utils.createRandomWord(3, new Random(Math.floor(Date.now()/4)).nextInt()),Utils.createRandomWord(4, new Random(Math.floor(Date.now()/5)).nextInt()),Utils.createRandomWord(5, new Random(Math.floor(Date.now()/6)).nextInt())]
         ]],
-        ["BR2048 (Glitch)",
+        ["BR2048 by Glitch",
         [
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ[]",
             "@#$_&-+()/",
             "§∆×÷"
         ]
         ],
-        ["GCI Layers (Glitch)",
+        ["GCI Layers by Glitch",
         [
             ["Grass", "Perks", "PP", "Crystals", "Grasshop", "Steel", "Charge", "Rocket Fuel", "Momentum", "Stars", "Fun", "Grass-Skip", "Dark Matter", "Planetarium", "Lines", "Arcs", "Stardust", "Solar Shards", "Solar Flares", "Solarians"],
             ["Loops", "Mega Loops", "Super Mega Loops", "Super Ultra Mega Loops", "Omega Loops"],
@@ -153,7 +153,7 @@ const mod = {
         ["Special Elite", "css/fonts/special-elite.css"],
         ["Courier", "css/fonts/courier.css"],
         ["Montserrat", "css/fonts/montserrat.css"],
-        ["Depixel (Hallbfett)", "css/fonts/depixelhbf.css"]
+        ["Depixel (Hallbfett)", "css/fonts/depixelhbf.css"],
         ["Rubik", "css/fonts/rubik.css"],
     ],
     saves: [
